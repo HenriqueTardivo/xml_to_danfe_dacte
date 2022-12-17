121540281a96ec33608240ad8932dbf9be066902
Henrique Tardivo <44455390+HenriqueTardivo@users.noreply.github.com>
2022-12-16 23:14:29 -0300

    melhorado codigo do utils

--- src/application/utils/MaskFields.ts
+++ src/application/utils/MaskFields.ts
@@ -1,88 +1,79 @@
 class MaskFields {
   maskCnpj(cnpj: string | undefined): string {
-    if (cnpj) {
-      return cnpj.replace(
-        /^(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})/,
-        "$1.$2.$3/$4-$5"
-      );
-    } else {
-      return "";
-    }
+    if (!cnpj) return "";
+
+    return cnpj.replace(
+      /^(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})/,
+      "$1.$2.$3/$4-$5"
+    );
   }
 
   maskIE(ie: string | undefined): string {
-    if (ie) {
-      return ie.replace(/(\d{3})?(\d{3})?(\d{3})?(\d{4})/, "$1.$2.$3-$4");
-    } else {
-      return "";
-    }
+    if (!ie) return "";
+
+    return ie.replace(/(\d{3})?(\d{3})?(\d{3})?(\d{4})/, "$1.$2.$3-$4");
   }
 
   maskCEP(cep: string | undefined): string {
-    if (cep) {
-      return cep.replace(/^([\d]{2})\.*([\d]{3})-*([\d]{3})/, "$1.$2-$3");
-    } else {
-      return "";
-    }
+    if (!cep) return "";
+
+    return cep.replace(/^([\d]{2})\.*([\d]{3})-*([\d]{3})/, "$1.$2-$3");
   }
 
   maskNumber(number: string | undefined): string {
     if (number) {
-      return String(
-        new Intl.NumberFormat("pt-BR", { minimumFractionDigits: 2 }).format(
-          Number(number)
-        )
-      );
-    } else {
       return "0,00";
     }
+
+    return String(
+      new Intl.NumberFormat("pt-BR", { minimumFractionDigits: 2 }).format(
+        Number(number)
+      )
+    );
   }
 
   maskDate(data: string | undefined): string {
-    if (data) {
-      if (data.length > 10)
-        return String(new Intl.DateTimeFormat("pt-br").format(new Date(data)));
-
-      return String(
-        new Intl.DateTimeFormat("pt-br").format(
-          new Date(data.replace(/-/g, "/"))
-        )
-      );
-    } else {
+    if (!data) {
       return "";
     }
+
+    if (data.length > 10)
+      return String(new Intl.DateTimeFormat("pt-br").format(new Date(data)));
+
+    return String(
+      new Intl.DateTimeFormat("pt-br").format(new Date(data.replace(/-/g, "/")))
+    );
   }
 
   maskTime(data: string | undefined): string {
-    if (data) {
-      if (data.length > 19) data = data.slice(0, 19);
+    if (!data) return "";
 
-      return String(
-        new Intl.DateTimeFormat("pt-br", {
-          hour: "numeric",
-          minute: "numeric",
-          second: "numeric",
-        }).format(new Date(data))
-      );
-    } else {
-      return "";
-    }
+    if (data.length > 19) data = data.slice(0, 19);
+
+    return String(
+      new Intl.DateTimeFormat("pt-br", {
+        hour: "numeric",
+        minute: "numeric",
+        second: "numeric",
+      }).format(new Date(data))
+    );
   }
 
   maskDateTime(data: string | undefined): string {
-    if (data) {
-      return String(this.maskDate(data) + " " + this.maskTime(data));
-    } else {
-      return "";
-    }
+    if (!data) return "";
+
+    return String(
+      new Intl.DateTimeFormat("pt-br", {
+        dateStyle: "short",
+        timeStyle: "medium",
+      }).format(new Date(data))
+    );
   }
 
   maskTelefone(telefone: string | undefined): string {
-    if (telefone) {
-      return telefone.replace(/^(\d{2})(\d{5})(\d{4})/, "($1)$2-$3");
-    } else {
-      return "";
-    }
+    if (!telefone) return "";
+
+    return telefone.replace(/^(\d{2})(\d{5})(\d{4})/, "($1)$2-$3");
   }
 }
 
